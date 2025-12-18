5de90957553cdae01032d768f88a43050f800d96
Carla_Diana <carladianavgonzales@gmail.com>
2025-12-18 13:44:08 -0400

    prueba

--- MicroServicesOrder/Controllers/OrderController.cs
+++ MicroServicesOrder/Controllers/OrderController.cs
@@ -1,4 +1,6 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using orderMicroService.Application.Services;
+using orderMicroService.Domain.Entities;
 
 namespace MicroServicesOrder.Controllers
 {
@@ -6,6 +8,62 @@ namespace MicroServicesOrder.Controllers
     [Route("[controller]")]
     public class OrderController : ControllerBase
     {
-        
+        private readonly OrderService _orderService;
+
+        public OrderController(OrderService orderService)
+        {
+            _orderService = orderService;
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> GetAll()
+        {
+            var orders = await _orderService.GetAll();
+            return Ok(orders);
+        }
+
+        [HttpGet("{id}")]
+        public async Task<IActionResult> GetById(int id)
+        {
+            var order = await _orderService.GetById(id);
+            if (order == null)
+            {
+                return NotFound();
+            }
+            return Ok(order);
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Create([FromBody] Order order, [FromHeader] int userId)
+        {
+            var created = await _orderService.Create(order, userId);
+            if (!created)
+            {
+                return BadRequest("Could not create order");
+            }
+            return StatusCode(201, order);
+        }
+
+        [HttpPut]
+        public async Task<IActionResult> Update([FromBody] Order order, [FromHeader] int userId)
+        {
+            var updated = await _orderService.Update(order, userId);
+            if (!updated)
+            {
+                return NotFound("Order not found or could not be updated");
+            }
+            return NoContent();
+        }
+
+        [HttpDelete("{id}")]
+        public async Task<IActionResult> Delete(int id, [FromHeader] int userId)
+        {
+            var deleted = await _orderService.DeleteById(id, userId);
+            if (!deleted)
+            {
+                return NotFound("Order not found or could not be deleted");
+            }
+            return NoContent();
+        }
     }
 }
